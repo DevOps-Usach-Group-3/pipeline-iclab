03f210951cae0184df4280ff5207e1f57910ef80
Christian Cordova <ccordova@yahoo.com>
2022-02-04 15:36:56 -0300

    Implementación inicial

---
+++ vars/ejecucion.groovy
@@ -0,0 +1,54 @@
+/*
+
+    forma de invocación de método call:
+
+    def ejecucion = load 'script.groovy'
+    ejecucion.call()
+
+*/
+
+def call(){
+  
+pipeline {
+    agent any
+    environment {
+        NEXUS_USER         = credentials('nexus-user-name')
+        NEXUS_PASSWORD     = credentials('nexus-user-pass')
+    }
+    parameters {
+        choice(
+            name:'compileTool',
+            choices: ['Maven', 'Gradle'],
+            description: 'Seleccione herramienta de compilacion'
+        )
+    }
+    stages {
+        stage("Pipeline"){
+            steps {
+                script{
+                  switch(params.compileTool)
+                    {
+                        case 'Maven':
+                            maven.call()
+                        break;
+                        case 'Gradle':
+                            gradle.call()
+                        break;
+                    }
+                }
+            }
+            post{
+                success{
+                    slackSend color: 'good', message: "[Su Nombre] [${JOB_NAME}] [${BUILD_TAG}] Ejecucion Exitosa", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-token'
+                }
+                failure{
+                    slackSend color: 'danger', message: "[Su Nombre] [${env.JOB_NAME}] [${BUILD_TAG}] Ejecucion fallida en stage [${env.TAREA}]", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-token'
+                }
+            }
+        }
+    }
+}
+
+}
+
+return this;
---
+++ vars/gradle.groovy
@@ -0,0 +1,50 @@
+/*
+	forma de invocación de método call:
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
+*/
+def call(){
+    stage("Paso 1: Build && Test"){
+        sh "gradle clean build"
+    }
+    stage("Paso 2: Sonar - Análisis Estático"){
+        sh "echo 'Análisis Estático!'"
+        withSonarQubeEnv('sonarqube') {
+            sh './gradlew sonarqube -Dsonar.projectKey=pipeline-iclab-prueba -Dsonar.java.binaries=build'
+        }
+    }
+    stage("Paso 3: Curl Springboot con Gradle durmiendo 20 segundos"){
+        sh "gradle bootRun&"
+        sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+    }
+    stage("Paso 4: Subir Nexus"){
+        nexusPublisher nexusInstanceId: 'nexus',
+        nexusRepositoryId: 'pipeline-iclab-prueba',
+        packages: [
+            [$class: 'MavenPackage',
+                mavenAssetList: [
+                    [classifier: '',
+                    extension: 'jar',
+                    filePath: 'build/libs/IClabPrueba-0.0.1.jar'
+                ]
+            ],
+                mavenCoordinate: [
+                    artifactId: 'IClabPrueba',
+                    groupId: 'com.devopsusach2020',
+                    packaging: 'jar',
+                    version: '0.0.1'
+                ]
+            ]
+        ]
+    }
+    stage("Paso 5: Descargar Nexus"){
+        sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/pipeline-iclab-prueba/com/devopsusach2020/IClabPrueba/0.0.1/IClabPrueba-0.0.1.jar" -O'
+    }
+    stage("Paso 6: Levantar Artefacto Jar"){
+        sh 'nohup bash java -jar IClabPrueba-0.0.1.jar & >/dev/null'
+    }
+    stage("Paso 7: Testear Artefacto durmiendo 20 segundos"){
+        sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+    }
+}
+return this;
